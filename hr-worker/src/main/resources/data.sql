6ee242e01f315396d15e53a31b668cdabebe7469
AntonioEvaristo <antonio.c.e.barbosa@outlook.com>
2021-09-15 20:38:43 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, dailylncome) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, dailylncome) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, dailylncome) VALUES ('Alex', 250.0);
