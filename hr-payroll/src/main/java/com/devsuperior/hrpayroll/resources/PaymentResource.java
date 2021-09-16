61b719c920ec76c289258b418b6b637e612b0694
AntonioEvaristo <antonio.c.e.barbosa@outlook.com>
2021-09-16 20:06:31 -0300

    hystrix fault tolerance

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.hrpayroll.resources;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,17 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = paymentService.getPayment(workerId,days);
         return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        return ResponseEntity.ok(Payment.builder()
+                .name("Brain")
+                .dailyIncome(400.00)
+                .days(days).build());
     }
 }
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -33,6 +33,12 @@ public class WorkerResource {
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findId(@PathVariable Long id){
 
+//        try {
+//            Thread.sleep(3000L);
+//        }catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+
         log.info("Port ="+environment.getProperty("local.server.port"));
 
         return ResponseEntity.ok(workerRepository.findById(id).get());
