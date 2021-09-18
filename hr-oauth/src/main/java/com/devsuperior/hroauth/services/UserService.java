c2c146a5de71f6ece4375375cdea4b199c8bc227
AntonioEvaristo <antonio.c.e.barbosa@outlook.com>
2021-09-17 21:35:48 -0300

    login e geração de tokem jwt

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -4,13 +4,16 @@ import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignclients.UserFeignClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 @Slf4j
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
@@ -21,6 +24,19 @@ public class UserService {
         if (!user.isPresent()) {
             log.error("Email not found, "+email);
             throw new IllegalArgumentException("Email not found");
+        }
+
+        log.info("Email found: "+email);
+        return user.get();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = Optional.ofNullable(userFeignClient.findByEmail(email).getBody());
+
+        if (!user.isPresent()) {
+            log.error("Email not found, "+email);
+            throw new UsernameNotFoundException("Email not found");
         }
 
         log.info("Email found: "+email);
